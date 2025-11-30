b3beda7e13248f0f0a85ce5fb5e58902e9a61e08
blaze-of-fire <carnold@byupathway.edu>
2025-11-29 23:36:16 -0700

    login styles finished

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,25 @@
-import LoginForm from '../component/login-form';
+import LoginForm from '@/app/component/login-form';
 import { Suspense } from 'react';
- 
+import Header from "@/app/component/header";
+import styles from "@/app/page.module.css"
+
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
+    <>
+      <div className={styles.page}>
+        <Header />
+        <main className={styles.main}>
+          <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+            <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+              <div className="w-32 text-white md:w-36">
+              </div>
+            </div>
+            <Suspense>
+              <LoginForm />
+            </Suspense>
           </div>
-        </div>
-        <Suspense>
-          <LoginForm />
-        </Suspense>
+        </main>
       </div>
-    </main>
+    </>
   );
 }
